9d722533612ba9e4213c85d1b7ebc3fa9b616a20
Gautam Makwana <makwanagautam411@gmail.com>
2025-12-24 21:12:40 +0530

    feat: add error handling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
+import { globalErrorHandler } from "./middlewares/globalErroHandler";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
     message: `this is server root route ➡️ Request handled by PORT: ${process.env.PORT}`,
   });
 });
+
+app.use("/api", routes);
+
+app.use(globalErrorHandler);
 
 export default app;
